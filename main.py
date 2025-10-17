28d7663ddec8237afa6e46b00ca80bc30558b47d
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-10-18 02:20:30 +0900

    이미지를 불러오기 위해 캔버스를 그리지만 안됨

--- main.py
+++ main.py
@@ -1,9 +1,56 @@
 from pico2d import *
+import random
 
 class warrior:
-    def __init__(self, x, y, z):
-        pass
+    def __init__(self):
+        self.x, self.y = 400, 300
+        self.frame = 0
+        self.image = load_image('51545.png')
     def update(self):
+
         pass
     def draw(self):
-        pass
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        #파일 안에 이미지 불러오기
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+    open_canvas()
+
+def reset_world():
+
+    global running
+    running = True
+
+    global world  # 모든 게임객체를 담을 수 있는 리스트
+    world = []
+
+    Warrior = warrior()
+    world.append(Warrior)
+
+reset_world()
+
+def update_world():
+    for game_object in world:
+        game_object.update()
+
+def render_world():
+    clear_canvas()
+    for game_object in world:
+        game_object.draw()
+    update_canvas()
+
+while running:
+    handle_events()
+
+    update_world()  # 객체들의 상호작용을 시뮬레이션 , 계산
+    render_world()  # 객체들의 모습을 그린다.
+
+close_canvas()
