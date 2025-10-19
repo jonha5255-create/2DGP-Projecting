49810eefbea93ba81ca8710bf84b60ca112dc192
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-10-19 14:42:16 +0900

    공격 모션 클래스 추가 및 수정

--- warrior.py
+++ warrior.py
@@ -1,16 +1,50 @@
 from pico2d import load_image
-from sdl2 import SDL_BUTTON_LEFT, SDL_BUTTON_RIGHT
+from sdl2 import SDL_BUTTON_LEFT, SDL_BUTTON_RIGHT, SDL_MOUSEBUTTONDOWN, SDL_KEYDOWN, SDLK_LEFT
 
+from state_machine import StateMachine
+
+
+class IDLE:
+    def __init__(self,warrior):
+        self.warrior = warrior
+        self.frame = 0
+        self.image = load_image('warrior idle.png')
+    def update(self):
+        self.warrior.frame += 1
+        self.warrior.frame = (self.warrior.frame + 1) % 3
+    def draw(self):
+        self.image.clip_draw(self.warrior.frame * 100 ,0, 100, 100, self.warrior.x, self.warrior.y)
+
+class ATTACK:
+    def __init__(self,warrior):
+
+        self.warrior = warrior
+        self.warrior.frame = 0
+        self.warrior.image = load_image('warrior attack.png')
+    def update(self):
+        self.warrior.frame += 1
+        self.warrior.frame = (self.warrior.frame + 1) % 4
+    def draw(self):
+        self.image.clip_draw(self.warrior.frame * 100 ,0, 100, 100, self.warrior.x, self.warrior.y)
+    pass
 
 class warrior:
     def __init__(self):
         self.x, self.y = 100, 200
         self.frame = 0
-        self.image = load_image('warrior idle.png')
+
+        self.IDLE = IDLE(self)
+        self.ATTACK = ATTACK(self)
+        self.state_machine = StateMachine (
+            self.IDLE,
+            {
+                self.IDLE : {('INPUT',): self.ATTACK},
+                self.ATTACK : {('INPUT',): self.IDLE,}
+            }
+        )
+
     def update(self):
-        self.frame += 1
-        self.frame = (self.frame + 1) % 3
+        self.state_machine.update()
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100 ,0, 100, 100, self.x, self.y)
-        #파일 안에 이미지 불러오기
+        self.state_machine.draw()
