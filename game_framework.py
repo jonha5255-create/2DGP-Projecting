d82f1d7a528606a9f8b94420d7019ff117d0caf0
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-11-12 01:23:00 +0900

    framework에 있는 함수 코드 구현

--- game_framework.py
+++ game_framework.py
@@ -1,14 +1,31 @@
 import time
 frame_time = 0.0
 
+running = None
+stack = None
+
 def change_mode(mode):
-    pass
+    global stack
+    if (len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
+    stack.append(mode)
+    mode.init()
 
 def push_mode(mode):
-    pass
+    global stack
+    if(len(stack) > 0):
+        stack[-1].pause()
+    stack.append(mode)
+    mode.init()
 
 def pop_mode():
-    pass
+    global stack
+    if(len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
+    if(len(stack) > 0):
+        stack[-1].resume()
 
 def quit():
     global running
