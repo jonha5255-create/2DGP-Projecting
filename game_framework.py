f9996f15ebe80c866456a5303ee01c4eadd5f9f2
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-11-12 01:23:45 +0900

    나머지 코드 구현하기

--- game_framework.py
+++ game_framework.py
@@ -48,3 +48,7 @@ def run(start_mode):
         frame_time = time.time() - current_time
         frame_time = 1.0/frame_time
         current_time += frame_time
+
+    while(len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
