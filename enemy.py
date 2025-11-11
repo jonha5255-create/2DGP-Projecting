2b7b3b05e3c1a1ee7556016db72ba74495b55e9d
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-11-12 03:24:15 +0900

    IDLE과 공격 모션 나눠 놓기

--- enemy.py
+++ enemy.py
@@ -1,16 +1,40 @@
 from pico2d import *
 import game_world
 
+from state_machine import StateMachine
+
+class IDLE:
+    def __init__(self, enemy):
+        self.enemy = enemy
+        self.image = load_image('enemy1.png')
+
+    def enter(self,e):
+        self.enemy.frame = 0
+
+    def exit(self,e):
+        pass
+
+    def do(self):
+       self.enemy.frame = (self.enemy.frame + 1) % 4
+
+    def draw(self):
+        self.image.clip_draw(self.enemy.frame * 37 ,0, 37, 100, self.enemy.x, self.enemy.y, 60, 60)
+
+
 
 class enemy:
     def __init__(self):
         self.x, self.y = 1200, 180
         self.frame = 0
-        self.image = load_image('enemy1.png')
+
+        self.enemy_idle = IDLE(self)
+        self.state_machine = self.enemy_idle
+
     def update(self):
-        self.frame += 1
-        self.frame = (self.frame + 1) % 4
+        self.state_machine.update()
 
     def draw(self):
-        self.image.clip_draw(self.frame * 37 ,0, 37, 100, self.x, self.y, 60, 60)
-        #파일 안에 이미지 불러오기
+        self.state_machine.draw()
+
+    def handle_event(self, event):
+        self.state_machine.handle_state_event(('INPUT',event))
