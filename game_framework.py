3a0656f93002d7bc3fd843a4c1b5d82493b94b3d
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-11-12 16:51:01 +0900

    프레임워크 파일 수정

--- game_framework.py
+++ game_framework.py
@@ -1,3 +1,4 @@
+from pico2d import *
 import time
 frame_time = 0.0
 
@@ -14,17 +15,17 @@ def change_mode(mode):
 
 def push_mode(mode):
     global stack
-    if(len(stack) > 0):
+    if len(stack) > 0:
         stack[-1].pause()
     stack.append(mode)
     mode.init()
 
 def pop_mode():
     global stack
-    if(len(stack) > 0):
+    if len(stack) > 0:
         stack[-1].finish()
         stack.pop()
-    if(len(stack) > 0):
+    if len(stack) > 0:
         stack[-1].resume()
 
 def quit():
@@ -45,10 +46,11 @@ def run(start_mode):
         stack[-1].handle_events()
         stack[-1].update()  # 객체들의 상호작용을 시뮬레이션 , 계산
         stack[-1].draw()  # 객체들의 모습을 그린다.
+
         frame_time = time.time() - current_time
         frame_time = 1.0/frame_time
         current_time += frame_time
 
-    while(len(stack) > 0):
+    while len(stack) > 0:
         stack[-1].finish()
         stack.pop()
