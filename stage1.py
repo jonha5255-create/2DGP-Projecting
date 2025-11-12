41a6bd9fb89a34cdeebca9bd31b90319b9c4ce0a
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-11-12 17:27:18 +0900

    배경 움직이는 로직 추가로 모든 배경이 움직이게 하였다.

--- stage1.py
+++ stage1.py
@@ -8,9 +8,11 @@ class stage1:
 
         self.water_x = 0
         self.cloud_x = 0
+        self.ground_x = 0
 
         self.water_speed = 2
         self.cloud_speed = 1
+        self.ground_speed = 3
 
     def draw(self):
         self.cloud.draw(self.cloud_x, 350)
@@ -19,9 +21,19 @@ class stage1:
         self.water.draw(self.water_x, 200)
         self.water.draw(self.water_x + 900,200)
 
-        self.ground.draw(450, 200)
+        self.ground.draw(self.ground_x, 200)
+        self.ground.draw(self.ground_x + 900,200)
 
     def update(self):
-        self.water_x += self.water_speed
+        self.water_x -= self.water_speed
         if self.water_x <= -self.water.w:
             self.water_x = 0
+
+        self.cloud_x -= self.cloud_speed
+        if self.cloud_x <= -self.cloud.w:
+            self.cloud_x = 0
+
+        self.ground_x -= self.ground_speed
+        if self.ground_x <= -self.ground.w:
+            self.ground_x = 0
+
