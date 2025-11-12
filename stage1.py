7a8db99d06314b9f4b80e9763aadd594b1229f1a
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-11-12 17:23:48 +0900

    배경을 추가해 캔버스가 안보이게 만들기 및 속도를 주어 배경이 움직이는것처럼 만들기

--- stage1.py
+++ stage1.py
@@ -2,10 +2,26 @@ from pico2d import *
 
 class stage1:
     def __init__(self):
-        self.image = load_image('stage1_background.png')
+        self.ground = load_image('stage1_background.png')
+        self.water = load_image('stage1_backwater.png')
+        self.cloud = load_image('stage1_backcloud.png')
+
+        self.water_x = 0
+        self.cloud_x = 0
+
+        self.water_speed = 2
+        self.cloud_speed = 1
 
     def draw(self):
-        self.image.draw(450, 150)
+        self.cloud.draw(self.cloud_x, 350)
+        self.cloud.draw(self.cloud_x + 900, 350)
+
+        self.water.draw(self.water_x, 200)
+        self.water.draw(self.water_x + 900,200)
+
+        self.ground.draw(450, 200)
 
     def update(self):
-        pass
+        self.water_x += self.water_speed
+        if self.water_x <= -self.water.w:
+            self.water_x = 0
