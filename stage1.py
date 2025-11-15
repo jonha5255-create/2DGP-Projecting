034f49088d761c8254759a18c9ee266af9dc5b2b
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-11-15 20:13:19 +0900

    배경 자연스럽게 움직이는 로직 완료

--- stage1.py
+++ stage1.py
@@ -6,34 +6,38 @@ class stage1:
         self.water = load_image('stage1_backwater.png')
         self.cloud = load_image('stage1_backcloud.png')
 
+        self.water_width = self.water.w
+        self.cloud_width = self.cloud.w
+        self.ground_width = self.ground.w
+
         self.water_x = 0
         self.cloud_x = 0
         self.ground_x = 0
 
-        self.water_speed = 2
-        self.cloud_speed = 1
-        self.ground_speed = 3
+        self.water_speed = 4
+        self.cloud_speed = 2
+        self.ground_speed = 6
 
     def draw(self):
-        self.cloud.draw(self.cloud_x + 450, 350)
-        self.cloud.draw(self.cloud_x + 1350, 350)
+        for i in range(3):
+            self.cloud.draw(self.cloud_x + i * self.cloud_width, 350)
 
-        self.water.draw(self.water_x + 450, 200)
-        self.water.draw(self.water_x + 1350,200)
+        for i in range(3):
+            self.water.draw(self.water_x + i * self.water_width, 200)
 
-        self.ground.draw(self.ground_x + 450, 200)
-        self.ground.draw(self.ground_x + 1350, 200)
+        for i in range(3):
+            self.ground.draw(self.ground_x + i * self.ground_width, 200)
 
     def update(self):
-        #self.water_x -= self.water_speed
-        #if self.water_x <= -900:
-        #    self.water_x = 0
+        self.water_x -= self.water_speed
+        if self.water_x <= -self.water_width:
+            self.water_x = 0
 
-       # self.cloud_x -= self.cloud_speed
-       # if self.cloud_x <= -900:
-       #     self.cloud_x = 0
+        self.cloud_x -= self.cloud_speed
+        if self.cloud_x <= -self.cloud_width:
+            self.cloud_x = 0
 
-       # self.ground_x -= self.ground_speed
-       # if self.ground_x <= -900:
-       #     self.ground_x = 0
+        self.ground_x -= self.ground_speed
+        if self.ground_x <= -self.ground_width:
+            self.ground_x = 0
         pass
