08ec940bab45fd942c937de1c23da891c7ef35b4
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-11-20 13:54:30 +0900

    직업별 블록스킬중 랜덤으로 가져오게 하기위한 로직

--- skill_block.py
+++ skill_block.py
@@ -4,8 +4,16 @@ class SKILLBLOCK:
     BLOCK_WIDTH = 125
     BLOCK_HEIGHT = 90
 
+    JOB_SKILL = {
+        'warrior' : 'warrior_skill.png',
+        'archer' : 'archer_skill.png',
+        'healer' : 'healer_skill.png'
+    }
     def __init__(self):
-        self.image = load_image('skill_block.png')
+        random_job = random.choice(['warrior', 'archer', 'healer'])
+        self.skill_type = random_job
+
+        image_file = self.JOB_SKILL.get(skill_type, '.png')
         self.x, self.y = 700, 90
 
     def draw(self):
