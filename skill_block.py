9743380f3761c4832f74e1f607e3ad68f9f05c8c
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-11-20 14:50:32 +0900

    스킬 생성에 2초 딜레이 넣는 중

--- skill_block.py
+++ skill_block.py
@@ -2,13 +2,17 @@ import random
 
 from pico2d import *
 
+import game_framework
+
+
 class SKILLBLOCK:
     BLOCK_WIDTH = 125
     BLOCK_HEIGHT = 90
     START_X = 1170  # 최종 목표 x 좌표
     START_Y = 50   # 최종 목표 y 좌표
-    SPACING = 10   # 블록 간 간격
-    SPEED = 100     # 이동 속도
+    SPACING = 5   # 블록 간 간격
+    SPEED = 100     # 이동 속도 100
+    SPAWN_DELAY = 2.0  # 생성 지연 시간
 
     JOB_SKILL = {
         'warrior': 'warrior_skill.png',
@@ -28,19 +32,33 @@ class SKILLBLOCK:
         self.y = self.START_Y
 
         # 목표 위치 (오른쪽으로 쌓이도록)
-        self.target_x = self.START_X + index * (self.BLOCK_WIDTH + self.SPACING)
+        self.target_x = self.START_X - index * (self.BLOCK_WIDTH + self.SPACING)
         self.target_y = self.START_Y
 
-        self.is_moving = True
+        self.spawn_delay = index * self.SPAWN_DELAY
+        self.spawn_timer = 0
+        self.is_spawned = False
+        self.is_moving = False
 
     def draw(self):
         self.image.draw(self.x, self.y, self.BLOCK_WIDTH, self.BLOCK_HEIGHT)
 
     def update(self):
+        # 생성 대기 중
+        if not self.is_spawned:
+            self.spawn_timer += game_framework.frame_time
+            if self.spawn_timer >= self.spawn_delay:
+                self.is_spawned = True
+                self.is_moving = True
+            return
+
+        # 이동 중
         if self.is_moving:
-            # 목표 위치로 이동
-            if self.x < self.target_x:
-                self.x += self.SPEED
-                if self.x >= self.target_x:
+            if self.x > self.target_x:
+                self.x -= self.SPEED * game_framework.frame_time
+                if self.x <= self.target_x:
                     self.x = self.target_x
                     self.is_moving = False
+
+    def has_arrived(self):
+        return not self.is_moving
