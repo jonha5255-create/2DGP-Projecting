056d68b3cfebe143819ce6ed393f2cef4eb46542
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-12-07 22:38:06 +0900

    스킬별 이펙트 로직 구현

--- effect.py
+++ effect.py
@@ -4,7 +4,7 @@ import game_world
 
 class EFFECT:
     IMAGE_FILE = {
-        'healer_heal': 'healer_att_effect.png',
+        'healer_heal': 'heal_effect.png',
         'warrior_attack': 'warrior_skill_effect.png',
         'archer_attack': 'archer_effect.png'
     }
@@ -15,6 +15,27 @@ class EFFECT:
         self.y = y
         self.scale = scale
         self.timer = 0.0
+
+        if effect_type == 'healer_heal':
+            self.frame_count = 3
+            self.duration = 0.3
+            self.frame_width = 100
+            self.frame_height = 100
+            self.is_animated = True
+        elif effect_type == 'warrior_attack':
+            self.frame_count = 4
+            self.duration = 0.4
+            self.frame_width = 128
+            self.frame_height = 100
+            self.is_animated = True
+        elif effect_type == 'archer_attack':
+            self.frame_count = 3
+            self.duration = 0.4
+            self.frame_width = 120
+            self.frame_height = 100
+            self.is_animated = True
+
+        self.current_frame = 0
 
     def update(self):
         pass
