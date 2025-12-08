1ad7542fe5e251e65fe120c24e4a9ad0f4f05bfd
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-12-08 19:06:58 +0900

    상수로 수정

--- level_manager.py
+++ level_manager.py
@@ -30,13 +30,13 @@ class LEVEL_MANAGER:
             for i in range(count):
                 mob = enemy()
                 mob.x = 1000 + i * 50
-                mob.hp = enemy.hp + (self.stage * 20)
+                mob.hp = 100 + (self.stage * 20)
                 game_world.add_object(mob, 1)
                 enemies.append(mob)
         else:
             boss_mob = boss()
             boss_mob.x = 1100
-            boss_mob.hp = boss.hp + (self.stage * 100)
+            boss_mob.hp = 1000 + (self.stage * 100)
             game_world.add_object(boss_mob, 1)
             enemies.append(boss_mob)
 
