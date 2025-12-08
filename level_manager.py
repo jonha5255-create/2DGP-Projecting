2293cc872a5297b4ffbed11dbac196a8422e7b24
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-12-08 18:26:40 +0900

    웨이브 시스템및 스테이지 클리어후 스테이지 넘어가기

---
+++ level_manager.py
@@ -0,0 +1,25 @@
+import random
+from pico2d import *
+import game_world
+
+from enemy1 import enemy
+from boss import boss
+from stage1 import stage1
+from stage2 import stage2
+from stage3 import stage3
+
+class LEVEL_MANAGER:
+    def __init__(self):
+        pass
+
+    def get_current_stage(self, level):
+        pass
+
+    def spawn_wave(self):
+        pass
+
+    def check_wave_status(self, dt):
+        pass
+
+    def next_stage(self):
+        pass
