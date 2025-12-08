b0bc98f30552087a0bccf2d0e6987d72260af781
kimjunha-notebook <wnsgk5255@tukorea.ac.kr>
2025-12-09 00:10:17 +0900

    적 ai 구현중

--- enemy1.py
+++ enemy1.py
@@ -2,6 +2,7 @@ from pico2d import *
 
 import game_framework
 import game_world
+import heroes
 
 from state_machine import StateMachine
 
@@ -15,6 +16,7 @@ class ATTACK:
 
     def enter(self,e):
         self.enemy.frame = 0
+        self.timer = 0.0
 
     def exit(self,e):
         pass
@@ -24,17 +26,24 @@ class ATTACK:
         if self.timer > 0.1:
             self.enemy.frame = (self.enemy.frame + 1) % 7
             self.timer = 0.0
-        pass
 
-    def update(self):
-        pass
+        # 공격 동작이 한 사이클 끝나면
+        if self.enemy.frame == 6:
+            target = self.enemy.get_nearest_hero()
+            # 여전히 적이 사거리에 있으면 -> 계속 공격 (frame 0으로 리셋)
+            if target and abs(target.x - self.enemy.x) < 50:
+                self.enemy.frame = 0
+            else:
+                # 적이 멀어졌거나 죽었으면 -> 다시 RUN
+                self.enemy.state_machine.cur_state = self.enemy.enemy_run
+                self.enemy.enemy_run.enter(None)
 
     def draw(self):
         self.image.clip_draw(self.enemy.frame * 37 ,0, 37, 100, self.enemy.x, self.enemy.y, 60, 60)
         draw_rectangle(*self.enemy.get_bb())
 
 
-class IDLE:
+class RUN:
     def __init__(self, enemy):
         self.enemy = enemy
         self.image = load_image('enemy1.png')
@@ -51,7 +60,17 @@ class IDLE:
         if self.timer >= 0.1:
             self.enemy.frame = (self.enemy.frame + 1) % 4
             self.timer = 0.0
-        pass
+
+        # 2. 이동 (왼쪽으로)
+        self.enemy.x -= 100 * game_framework.frame_time  # 이동 속도
+
+        # 3. 타겟 탐색 및 상태 전이
+        target = self.enemy.get_nearest_hero()
+        if target:
+            # 사거리 50 픽셀 이내면 공격 상태로 전환
+            if abs(target.x - self.enemy.x) < 50:
+                self.enemy.state_machine.cur_state = self.enemy.enemy_attack
+                self.enemy.enemy_attack.enter(None)
 
     def draw(self):
         self.image.clip_draw(self.enemy.frame * 37 ,0, 37, 100, self.enemy.x, self.enemy.y, 60, 60)
@@ -68,16 +87,18 @@ class enemy:
     def __init__(self):
         self.x, self.y = 1000, 180
         self.frame = 0
-        self.hp = 150
-        self.str = 20
+        self.hp = 100
+        self.str = 10
+        self.ATTACK_CLASS = ATTACK
 
-        self.enemy_idle = IDLE(self)
+        self.enemy_run = RUN(self)
         self.enemy_attack = ATTACK(self)
+
         self.state_machine = StateMachine(
-            self.enemy_idle,
+            self.enemy_run,
             {
-                self.enemy_attack : { 'INPUT': self.enemy_idle },
-                self.enemy_idle : { 'INPUT': self.enemy_attack }
+                self.enemy_attack : {},
+                self.enemy_run : {}
             }
         )
 
